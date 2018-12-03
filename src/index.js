45904929eaf738488488dd5741f6b40873a9de99
Denis <denis.sedura@gmail.com>
2018-12-03 11:18:36 +0200

    Update index.js
    Fix InteractionManager blocked by looping animation

--- src/index.js
+++ src/index.js
@@ -89,11 +89,13 @@ class ContentLoader extends Component {
     Animated.sequence([
       Animated.timing(this._animate, {
         toValue: 1,
-        duration: this.state.frequence
+        duration: this.state.frequence,
+        isInteraction: false
       }),
       Animated.timing(this._animate, {
         toValue: 0,
-        duration: this.state.frequence
+        duration: this.state.frequence,
+        isInteraction: false
       })
     ]).start(event => {
       if (event.finished) {
